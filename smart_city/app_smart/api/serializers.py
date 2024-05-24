f3cef0af13729eaa49cd76baad543a59a18c94ae
Thayn��� Alves Q <46876924866@>
2024-05-24 16:49:50 -0300

    Carregando dados dos sensores

--- smart_city/app_smart/api/serializers.py
+++ smart_city/app_smart/api/serializers.py
@@ -2,6 +2,8 @@
 from django.contrib.auth.models import User # Importando o models User já criado pelo django
 from rest_framework import serializers # importando o serializer do rest 
 from django.contrib.auth.hashers import make_password # permite criptografar a senha
+from app_smart.models import Sensor
+
 
 class UserSerializer (serializers.ModelSerializer):
 
@@ -16,3 +18,9 @@ class UserSerializer (serializers.ModelSerializer):
         fields = ['id', 'username', 'email', 'password']
     extra_kwargs = {'password':{'write_only': True} } # diz que se quiser consumir uma 
                                                       #senha ele não vai retorná-la
+
+
+class SensorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Sensor #tabela no banco de dados 
+        fields = '__all__' #está permitindo o tráfego de dados de todos os campos 
